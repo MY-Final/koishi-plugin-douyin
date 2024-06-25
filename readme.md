2db3fa65d87c9f7d37d70e9dda9eecfd7acb1f1c
tediorelee <tediorelee@leejieun.fan>
2024-06-25 22:26:10 +0800

    feat: init

--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 [![npm](https://img.shields.io/npm/v/koishi-plugin-douyin?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-douyin)
 
 koishi plugin for douyin
+
+解析群聊的douyin视频分享链接
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,53 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, h, segment } from 'koishi'
+import { resolve } from 'path'
+import {} from '@koishijs/plugin-console'
 
-export const name = 'douyin'
+export const name = 'xiaohongshu'
 
-export interface Config {}
+export interface Config {
+  apikey: string;
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config = Schema.object({
+  description: Schema.string().default('api参考, 可按量购买').description('https://shorturl.at/2rATh'),
+  apikey: Schema.string().default('').required().description('填写douyin解析的apikey'),
+})
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export const api = 'https://api.mu-jie.cc/douyin'
+
+export function apply(ctx: Context, config: Config) {
+
+  async function fetchFromAPI(url) {
+    return await ctx.http.get(`${api}?url=${url}&key=${config.apikey}`);
+  };
+
+  ctx.middleware(async (session, next) => {
+    if (!session.content.includes('douyin.com')) return next()
+    const urlRegex = /(https?:\/\/[^\s]+)/g;
+    const url = session.content.match(urlRegex)[0];
+    if (!url) return
+
+    try {
+      const result = await fetchFromAPI(url);
+      if (result.code !== 200) {
+        return '解析失败!';
+      }
+      const type = result.data.type
+      const cover = result.data.cover || '';
+
+      if (type === "视频") {
+        session.send(h.video(result.data.url))
+      }
+    } catch(err) {
+      console.log(err);
+      return `发生错误!;  ${err}`;
+    }
+  });
+
+  ctx.inject(['console'], (ctx) => {
+    ctx.console.addEntry({
+      dev: resolve(__dirname, '../client/index.ts'),
+      prod: resolve(__dirname, '../dist'),
+    })
+  })
 }
